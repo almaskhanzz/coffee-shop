b57db66fcae985b2b71b236d5edd03a85d85b3c7
Almas Khan <almas.rbm@gmail.com>
2022-03-26 01:41:35 +0600

    header updated

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            <nav className='header'>
+                <h1>Wake Up Cafe</h1>
+                <h3>The Best Coffee in Town</h3>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
